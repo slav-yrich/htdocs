de6bb8f39b159ae91dec20b304dcf9e7d9104ee2
Anna Iliyina <anna.iliyina@gmail.com>
2014-12-10 15:54:41 +0400

    есть даты в D.M.Y, нет вывода аватарок

--- pages/lk.php
+++ pages/lk.php
@@ -1,5 +1,5 @@
 
-	<script language="javascript">
+<script language="javascript">
 function doPopup(popupPath) {
 window.open(popupPath,'name',
 'width=850,height=550,scrollbars=YES');
@@ -19,7 +19,12 @@ $sql1=pg_query($query1);
 $row1=pg_fetch_array($sql1);
 $query2="SELECT * FROM department WHERE id=".$row['department'];
 $sql2=pg_query($query2);
-$row2=pg_fetch_array($sql2);?>
+$row2=pg_fetch_array($sql2);
+
+function FormatDate($date)                        {
+	   $newDate =  DateTime::createFromFormat('Y-m-d', $date) -> format('d-m-Y');
+return $newDate;
+	}?>
 
 			<div class="row-fluid">
 				<div class="span6">
@@ -44,7 +49,7 @@ $row2=pg_fetch_array($sql2);?>
 					            </tr>
 					            <tr>
 					            	<td>Дата рождения:</td>
-					            	<td class="td"><?php echo $row['datebirth'];?></td>
+					            	<td class="td"><?php echo FormatDate($row['datebirth']);?></td>
 					            </tr>
 					            <tr>
 					            	<td>Организация:</td>
@@ -66,19 +71,16 @@ $row2=pg_fetch_array($sql2);?>
       							 <th>Дата</th><th>Автор</th><th>Наименование</th><th>Штрих-код</th>
      <?php $query3="SELECT * FROM event WHERE status = 2 AND barcode_reader=".$row['barcode'];
 $sql3=pg_query($query3) or die('<tr><td>-</td><td>-</td><td>-</td><td>-</td><td>-</td></tr>'	);
-
 for ($i=1; $i <= pg_num_rows($sql3) ; $i++)
   {
 $row3=pg_fetch_array($sql3);
-
 $query4="SELECT * FROM book WHERE  barcode_book=".$row3['barcode_book'];
 $sql4=pg_query($query4)or die(pg_error() );
 $row4=pg_fetch_array($sql4);
 $query5="SELECT * FROM author WHERE id=".$row4['author'];
 $sql5=pg_query($query5);
 $row5=pg_fetch_array($sql5)or die(pg_error() );
-echo '<tr><td>'.$row3['date_start'].'</td><td>'.$row5['author_fio'].'</td><td>'.$row4['title'].'</td><td>'.$row3['barcode_book'].'</td></tr>';
-
+echo '<tr><td>'.FormatDate($row3['date_start']).'</td><td>'.$row5['author_fio'].'</td><td>'.$row4['title'].'</td><td>'.$row3['barcode_book'].'</td></tr>';
 }
 ?>
  						</table><br>
