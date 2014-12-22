e7032610c1a10586582cd70a945e2bc22b4378dc
slav-yrich <slav-yrich@rambler.ru>
2014-12-23 00:50:23 +0300

    Revert "Revert "Поиск с защитой от SQL инъекций""
    This reverts commit 4d29b95e7f5e2db9d3605b849d0abeeb65c161cd.

--- pages/poisk_next.php
+++ pages/poisk_next.php
@@ -3,6 +3,7 @@
 				<div class="span6">
 					
 				<ul class="thumbnails newBooks">
+					<div class="clearfix">
 					<?php
 						if ($_POST["avtor"]!='')
 						{
@@ -33,61 +34,56 @@
 
 						function poisk($avtor, $nazvanie, $kategor)
 						{
-							if ($avtor=='')
-							{
-								$avtor='%';
-							}
-							if ($nazvanie=='')
-							{
-								$nazvanie='%';
-							}
-							if ($kategor=='Каталог литературы')
-							{
-								$kategor='%';
-							}
+							$avtor='%'.pg_escape_string($avtor).'%'; 
+							$nazvanie='%'.pg_escape_string($nazvanie).'%'; 
+							if ($kategor=='Каталог литературы') 
+								{ $kategor='%'; }
+							else 
+								{ $kategor=pg_escape_string($kategor);}
 
-							$result = pg_query("SELECT * FROM book 
-												WHERE author IN (SELECT id FROM author WHERE author_fio LIKE '%$avtor%')
-													AND category IN (SELECT id FROM category WHERE category_name LIKE '%$kategor%')
-													AND title LIKE '%$nazvanie%' ");
-							$handle = pg_query("SELECT count(1) FROM book 
-												WHERE author IN (SELECT id FROM author WHERE author_fio LIKE '%$avtor%')
-													AND category IN (SELECT id FROM category WHERE category_name LIKE '%$kategor%')
-													AND title LIKE '%$nazvanie%' ");
-							$tmp  = pg_fetch_array($handle);
-							$k=$tmp[0];
+							$sql = "SELECT * FROM book 
+									WHERE author IN (SELECT id FROM author WHERE author_fio LIKE '%$avtor%')
+											AND category IN (SELECT id FROM category WHERE category_name LIKE '%$kategor%')
+											AND title LIKE '%$nazvanie%' ";
+
+							$result = pg_query($sql) or die(pg_error());
 
 							echo '<h1>Результаты поиска:</h1>';
-							
-							if($k>0)
+							$k=0;
+
+							while ($current_book=pg_fetch_array($result))
 							{
-								echo 'По вашему запросу найдены следующие книги:'.'<br>';
-								for ($i=0;$i<$k;$i++)
-								{
-									$book=pg_fetch_array($result);
-									//$cur_result= pg_query("SELECT * FROM author WHERE id=$book['author']");
-									//$cur_author = pg_fetch_array($cur_result);
-									echo "(code: ".$book['barcode_book'].
-									 	", название: ".$book['title'].
-									 	", автор: ".$book['author']./*$cur_author['author_fio'].*/
-									 	")<br>";
-								}
-								
-								echo "Всего найдено:".$k."<br>";	
+								if ($k++==0) echo 'По вашему запросу найдены следующие книги:'.'<br>';
+								echo '<li>'.
+									'<a href="/index.php?p=annot&amp;num_book='.$current_book['id_book'].'"><p>'.
+									$k.'| '.
+									'(barcode: '.$current_book['barcode_book'].
+									', название: '.$current_book['title'].
+									', автор: '.$current_book['author'].
+									')</p></a>'.
+									'</li>';
+							}
+							if ($k==0)
+							{
+								Echo "Сожалеем, но в базе нет такой книги. Попробуйте расширить условия поиска.";
 							}
 							else
 							{
-								Echo "Сожалеем, но в базе нет такой книги. Попробуйте расширить условия поиска.";
+								echo "Всего найдено:".$k."<br>";
 							}
 						}
 
 		poisk($avtor, $nazvanie, $kategor);
-		echo '<form action="poisk.php" method="post">'.
+		echo '<form action="index.php?p=poisk" method="post">'.
 					'<input name="net" type="submit" value="Назад">'.
 					'</form>';	
 			
 	?>
 				</div>
+			</ul>
+			</div>
+			</div>
+			
 
 
 					
