58db21524992a780733c9e51d4981b1cd6e540b7
glazatkina <glazatkina@gmail.com>
2015-01-21 12:56:50 +0400

    Update edit_next.php

--- pages/edit_next.php
+++ pages/edit_next.php
@@ -1,16 +1,16 @@
-﻿<?php 
+<?php 
+pg_set_client_encoding("UTF-8");
+
 $id=$_POST['qw'];
 $query="SELECT * FROM reader WHERE id='$id'";
 $sql=pg_query($query);
 $row=pg_fetch_array($sql);
-
-
-
-
  
 $values = array(
-    'surname' => 'sur', 'name' => 'name', 'patronymic' => 'otch',
-    'datebirth' => 'bday',
+    'surname' => 'sur', 
+	'name' => 'name', 
+	'patronymic' => 'otch',
+    'datebirth' => 'bday'
 );
  
 $id = isset($_POST['qw']) ? (int) $_POST['qw'] : 0;
@@ -18,22 +18,28 @@ $id = isset($_POST['qw']) ? (int) $_POST['qw'] : 0;
 if(empty($id))
 {
     //qw не ввели, обновлять нечего.
+	echo "Данные не были введены.";
 }
 else
 {
     $query = '';
     foreach($values as $k=>$v)
-        if(($_POST[$v]) != $row[$k])
-            $query .= '`' . $k . '` = "' . pg_escape_string($_POST[$v]) . '", ';
+        if(($_POST[$v]) != $row[$k]) {
+            $query .= $k . ' = \''. pg_escape_string($_POST[$v]) . '\', '; // "name = 'newname' ,"
+		}
+		
     if(empty($query))
     {
         //ничего не заполнили, обновлять нечего.
+		echo "Данные не были введены.";
     }
     else
     {
-        $res = pg_query('UPDATE `reader`  SET ' . rtrim($query, ', ') .  '   WHERE `id` = ' . $id);
-        if($res && pg_affected_rows() > 1)
-            echo 'Данные изменены успешно';
+        $res = pg_query('UPDATE reader SET ' . rtrim($query, ', ') . ' WHERE id = ' . $id . ';');
+		if (!$res)
+			echo "Ошибка запроса.";
+		else
+			echo 'Число измененных строк: ' . pg_affected_rows($res) . '. Обновите страницу.';
     }
 }
 ?>
